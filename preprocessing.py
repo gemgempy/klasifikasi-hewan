905d7f7f592eefa570611bf2d347bd4192babf03
Gempar Bambang Godata <135794145+gemgempy@users.noreply.github.com>
2025-11-15 15:27:02 +0700

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,3 @@
-# preprocessing.py
-# Fungsi untuk load & preprocessing gambar sebelum masuk ke CNN
-
 from typing import Tuple
 import numpy as np
 from PIL import Image
@@ -9,14 +6,6 @@ from config import IMG_SIZE
 
 
 def load_image(file) -> Image.Image:
-    """
-    Menerima:
-    - path string / Path
-    - file-like object dari st.file_uploader
-    - PIL.Image.Image
-
-    dan mengembalikan PIL image dalam mode RGB.
-    """
     if isinstance(file, Image.Image):
         img = file
     else:
@@ -25,20 +14,11 @@ def load_image(file) -> Image.Image:
 
 
 def preprocess_image(file) -> Tuple[np.ndarray, Image.Image]:
-    """Load gambar, resize, normalisasi.
-
-    Sama dengan ImageDataGenerator:
-      rescale=(1.0/127.5)-1.0 -> range piksel [-1, 1]
-
-    Output:
-      - image_array: np.ndarray shape (1, H, W, 3)
-      - img_resized: PIL.Image untuk ditampilkan di Streamlit
-    """
     img = load_image(file)
     img_resized = img.resize(IMG_SIZE)
 
     arr = np.asarray(img_resized).astype("float32")
-    arr = (arr / 127.5) - 1.0    # rescale ke [-1, 1]
-    arr = np.expand_dims(arr, axis=0)  # (1, H, W, 3)
+    arr = (arr / 127.5) - 1.0   # SAMA kayak ImageDataGenerator(rescale=(1/127.5)-1.0)
+    arr = np.expand_dims(arr, axis=0)
 
     return arr, img_resized
